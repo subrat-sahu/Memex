3b3e4d90ef1bafe77365f63e726cbe75935a059d
Subrat Sahu <ssubrat098@gmail.com>
2018-06-06 22:45:36 +0530

    Adds multiple element drag and drop 	- Sample UI 	- Needs some fixing

--- src/overview/actions.js
+++ src/overview/actions.js
@@ -102,7 +102,6 @@ export const search = ({ overwrite } = { overwrite: false }) => async (
     }
 
     dispatch(setLoading(true))
-    dispatch(listActs.resetUrlToEdit())
 
     if (showTooltip) {
         dispatch(fetchNextTooltip())
@@ -110,6 +109,7 @@ export const search = ({ overwrite } = { overwrite: false }) => async (
 
     // Overwrite of results should always reset the current page before searching
     if (overwrite) {
+        dispatch(listActs.resetUrlToEdit())
         dispatch(resetPage())
     }
 
